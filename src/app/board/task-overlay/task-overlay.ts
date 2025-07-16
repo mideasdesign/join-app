86ae60a1575ee7c50655a7bf6785a2aaa3486d78
Markus Fischer <fischer@mideasdesign.de>
2025-07-16 21:49:46 +0200

    feat: add task-overlay

--- src/app/board/task-overlay/task-overlay.ts
+++ src/app/board/task-overlay/task-overlay.ts
@@ -2,23 +2,24 @@ import { Component, Input, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TaskInterface } from '../../interfaces/task-interface';
-import { Firebase } from '../../shared/services/firebase-services';
-import { SuccessServices } from '../../shared/services/success-services';
+import { Firebase } from '../../Shared/firebase/firebase-services/firebase-services';
+import { SuccessServices } from '../../Shared/firebase/firebase-services/success-services';
+import { AddTask } from '../add-task/add-task';
 
 @Component({
   selector: 'app-task-overlay',
- imports: [CommonModule, ReactiveFormsModule],
+ imports: [CommonModule, ReactiveFormsModule, AddTask],
   templateUrl: './task-overlay.html',
   styleUrl: './task-overlay.scss'
 })
 
-export class TaskOverlay  implements OnInit{
+export class TaskOverlay{
   private success = inject(SuccessServices);
   private fb = inject(FormBuilder);
   private firebase = inject(Firebase);
 
   @Input() taskToEdit?: TaskInterface;
-  userList = ['peter müller', 'petra Heinz', 'Erik Huber'];
+
 
 contactsList = [
   { id: 'uid123', name: 'Heinz Müller', email: 'heinz@mail.de' },
