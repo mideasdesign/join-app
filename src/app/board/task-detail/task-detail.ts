c629c11881ac54885a22e79afcf6d77d381d0d17
Markus Fischer <fischer@mideasdesign.de>
2025-07-18 18:08:10 +0200

    feat: add task detail overlay view

--- src/app/board/task-detail/task-detail.ts
+++ src/app/board/task-detail/task-detail.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, Input, Output, EventEmitter, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup, FormArray, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { TaskInterface } from '../../interfaces/task-interface';
+import { TaskService } from '../../Shared/firebase/firebase-services/task-service';
+import { Firebase } from '../../Shared/firebase/firebase-services/firebase-services';
+import { SuccessServices } from '../../Shared/firebase/firebase-services/success-services';
+import { ContactsInterface } from '../../interfaces/contacts-interface';
 
 @Component({
   selector: 'app-task-detail',
-  imports: [FormsModule],
+ imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './task-detail.html',
   styleUrl: './task-detail.scss'
 })
-export class TaskDetail {
+export class TaskDetail implements OnInit {
+  private success = inject(SuccessServices);
+  private taskService = inject(TaskService);
+  private firebase = inject(Firebase);
+  public ContactsList: ContactsInterface[] = [];
+  @Input() selectedTask!: TaskInterface;
+  @Output() close = new EventEmitter<void>();
 
+  constructor() {}
+
+  ngOnInit() {
+    this.taskService.getContactsRef().subscribe((contacts: ContactsInterface[]) => {
+      this.ContactsList = contacts;
+    });
+  }
+
+  cancel() {
+    document.dispatchEvent(new CustomEvent('closeOverlay'));
+  }
+
+  trackByIndex(index: number) {
+    return index;
+  }
 }
